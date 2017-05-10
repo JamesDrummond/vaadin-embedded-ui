56354c639f21547a81991caa18a52bddc65b2894
James Drummond <jdrummond@codenvy.com>
2017-05-10 18:55:51 +0000

    fixup

--- dockerfiles/resources/entrypoint.sh
+++ dockerfiles/resources/entrypoint.sh
@@ -2,7 +2,10 @@
  set -e
  sed -i "s/8080/${NGINX_PORT_SERVE}/" /home/user/nginx/sites-available/ui_example_root
  mkdir -p /projects
- cp -rf /home/user/nginx /projects
+ ## Files in /projects folder are removed by codenvy when started
+ ## This workaround will copy nginx folder to /project once temp_workaround is removed
+ touch /projects/temp_workaround
+ bash -c "while [ -f /projects/temp_workaround ]; do wait 1 ; done; cp -rf /home/user/nginx /projects" &
  nginx -c /projects/nginx/nginx.conf
  /usr/sbin/sshd -f ~/.ssh/sshd_config -D &
  exec "$@"
