88f2f2487c4b0a4b836f559dddc9416ff3b3c758
James Drummond <jdrummond@codenvy.com>
2017-05-10 17:34:31 +0000

    fixup

--- dockerfiles/resources/entrypoint.sh
+++ dockerfiles/resources/entrypoint.sh
@@ -1,8 +1,4 @@
 #! /bin/bash
-
- sed -i "s/8080/${NGINX_PORT_SERVE}/" /home/user/nginx/sites-available/ui_example_root
- mkdir -p /projects
- cp -rf /home/user/nginx /projects
- nginx -c /projects/nginx/nginx.conf
+ set -e
  /usr/sbin/sshd -f ~/.ssh/sshd_config -D &
  exec "$@"
